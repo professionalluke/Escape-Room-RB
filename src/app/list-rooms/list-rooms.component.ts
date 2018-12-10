48b7aae6f4a24071ae24cabd70be95b649be0906
mrjasonalee <mrjasonalee@gmail.com>
2018-12-10 10:50:42 -0500

    test

--- src/app/list-rooms/list-rooms.component.ts
+++ src/app/list-rooms/list-rooms.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http'
 @Component({
   selector: 'app-list-rooms',
   templateUrl: './list-rooms.component.html',
